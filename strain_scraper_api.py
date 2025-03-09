f7aa7ca0948bb2b8f331d942f92da001f3edd49d
hempiesco <135643856+hempiesco@users.noreply.github.com>
2025-03-09 00:59:49 -0500

    Update strain_scraper_api.py

--- strain_scraper_api.py
+++ strain_scraper_api.py
@@ -1,7 +1,5 @@
-from flask_cors import CORS
-app = Flask(__name__)
-CORS(app)  # Allows all domains (for testing)
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
